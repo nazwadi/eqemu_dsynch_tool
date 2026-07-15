59acbaaea4c64ebfb5ffaa03032e8c3ccad8db5d
nazwadi <nazwadi@pm.me>
2026-07-14 23:46:01 -0400

    Minor CSS edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -141,7 +141,7 @@ function App() {
                         </div>
                     </div>
                 </div>
-                <div id="input" className="w-1/2 flex flex-col">
+                <div id="input" className="w-1/2 flex flex-1 flex-col">
                     <div className="justify-center">
                         <div
                             className="px-3 py-2 text-xs font-medium text-gray-400 uppercase tracking-wider border-b border-gray-700">
